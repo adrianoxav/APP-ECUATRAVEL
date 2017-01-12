0609feba1c256a69a90a915f2133c88dd59f9008
adrianoxav <apinargo@espol.edu.ec>
2017-01-12 12:44:25 -0500

    aniadido el primer prototipo de crear un plan de viaje a traves del cronograma del dispositivo

--- app/src/pages/cronograma/cronograma.ts
+++ app/src/pages/cronograma/cronograma.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Calendar} from 'ionic-native';
+import {ToastController} from 'ionic-angular';
 
 /*
   Generated class for the Cronograma page.
@@ -13,10 +15,58 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CronogramaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CronogramaPage');
-  }
 
+
+public title: string;
+public note: string;
+public startDate: Date;
+public endDate: Date;
+public titleUpdated: string;
+public noteUpdated: string;
+private nav: NavController;
+
+constructor( nav: NavController, public navParams: NavParams,private toastCtrl: ToastController) {
+
+  this.nav = nav;
+
+  this.title = 'Event 1';
+  this.note = 'Creating an event !';
+  this.startDate = new Date();
+  this.startDate.setMinutes(this.startDate.getMinutes() + 10);
+  this.endDate = new Date();
+  this.endDate.setHours(this.startDate.getHours() + 1);
+
+  this.titleUpdated = 'Event updated';
+  this.noteUpdated = 'We update the event !';
+}
+
+
+
+public createEvent():void{
+
+  let options:any = {
+    firstReminderMinutes:5
+  };
+
+  Calendar.createEventWithOptions(this.title, null, this.note, this.startDate, this.endDate, options).then(() => {
+    this.nav.push(this.toastCtrl.create({
+      message:'Created',
+      duration:3000
+    }));
+  });
+}
+
+public deleteEvent():void{
+  Calendar.deleteEvent(this.title, null, this.note, this.startDate, this.endDate).then(data => {
+    this.nav.push(this.toastCtrl.create({
+      message:data===true ? 'Deleted' : 'Not deleted',
+      duration:3000
+    }));
+  });
+}
+
+public openCalendar():void{
+  Calendar.openCalendar(this.startDate);
+}
 }

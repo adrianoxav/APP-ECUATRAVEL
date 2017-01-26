24e18cff94a56f1f940086c75ea218ed24ddf3da
adrianoxav <apinargo@espol.edu.ec>
2017-01-26 13:13:06 -0500

    aniadido primera iteracion de ingresar una experiencia con foto incluida

--- app/src/pages/lugares/lugares.ts
+++ app/src/pages/lugares/lugares.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ListPage } from '../list/list';
+import {Camera} from 'ionic-native';
+import { ActionSheetController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /*
   Generated class for the Lugares page.
@@ -14,10 +17,70 @@ import { ListPage } from '../list/list';
 })
 export class LugaresPage {
 
-
+  template: string = 'null';
     tab2Root: any = ListPage;
+    public base64Image: string;
 
-    constructor(){
+    constructor(public alertCtrl: AlertController){
 
     }
+    goCrear() {
+      this.template = 'crear';
+
+
+  }
+
+  cancelar() {
+
+      this.template = 'null';
+  }
+  takePicture(){
+   Camera.getPicture({
+       destinationType: Camera.DestinationType.DATA_URL,
+       targetWidth: 1000,
+       targetHeight: 1000
+   }).then((imageData) => {
+     // imageData is a base64 encoded string
+       this.base64Image = "data:image/jpeg;base64," + imageData;
+   }, (err) => {
+       console.log(err);
+   });
+ }
+
+ showPrompt() {
+     let prompt = this.alertCtrl.create({
+       title: 'Danos tu experiencia',
+       message: "Ingresa Tu comentario",
+       inputs: [
+         {
+           name: 'title',
+           placeholder: 'Title'
+         },
+       ],
+       buttons: [
+         {
+           text: 'Cancel',
+           handler: data => {
+             console.log('Cancel clicked');
+           }
+         },
+         {
+           text: 'Save',
+           handler: data => {
+             console.log('Saved clicked');
+           }
+         },
+         {
+          text: 'Foto',
+          handler: data => {
+            this.takePicture()
+          }
+           
+         }
+       ]
+     });
+     prompt.present();
+   }
+
+
   }

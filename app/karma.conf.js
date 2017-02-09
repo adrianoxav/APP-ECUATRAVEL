05be0157f9d115081807836bb0dcf68529ff2adf
adrianoxav <apinargo@espol.edu.ec>
2017-02-08 22:48:32 -0500

    tests aniadidos

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -1,58 +1,42 @@
-// Karma configuration
-// Generated on Wed Feb 08 2017 13:53:38 GMT-0500 (ECT)
-
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'angular-cli'],
-    files: [
-    'node_modules/es6-shim/es6-shim.js',        // TypeError: undefined is not a constructor (evaluating 'new exports.Map()')
-    'node_modules/reflect-metadata/Reflect.js', // 'Uncaught reflect-metadata shim is required when using class decorators'
-    'node_modules/zone.js/dist/zone.js',        // Zone.js dependencies (Zone undefined)
-    'node_modules/zone.js/dist/jasmine-patch.js',
-    'node_modules/zone.js/dist/async-test.js',
-    'node_modules/zone.js/dist/fake-async-test.js',
-    'app/**/*.spec.ts',
-    {pattern: 'node_modules/reflect-metadata/Reflect.js.map', included: false, served: true}, // 404 on the same
-    {pattern: 'www/build/**/*.html', included: false},
-  ],
     plugins: [
-     require('karma-jasmine'),
-     require('karma-chrome-launcher'),
-     require('karma-remap-istanbul'),
-     require('karma-mocha-reporter'),
-     require('angular-cli/plugins/karma')
-   ],
-   preprocessors: {
-        './src/test.ts': ['angular-cli']
-      },
-      mime: {
-        'text/x-typescript': ['ts','tsx']
-      },
-      remapIstanbulReporter: {
-        reports: {
-          html: 'coverage',
-          lcovonly: './coverage/coverage.lcov'
-        }
-      },
-      angularCli: {
-        config: './angular-cli.json',
-        environment: 'dev'
-      },
-      reporters: config.angularCli && config.angularCli.codeCoverage
-                ? ['mocha', 'karma-remap-istanbul']
-                : ['mocha'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: false
-    });
-  };
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-remap-istanbul'),
+      require('karma-mocha-reporter'),
+      require('angular-cli/plugins/karma'),
+      require('karma-phantomjs-launcher')
+    ],
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['angular-cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
+    },
+    remapIstanbulReporter: {
+      reports: {
+        html: 'coverage',
+        lcovonly: './coverage/coverage.lcov'
+      }
+    },
+    angularCli: {
+      config: './angular-cli.json',
+      environment: 'dev'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['mocha', 'karma-remap-istanbul']
+              : ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  });
+};
--- app/src/app/app.spec.ts
+++ app/src/app/app.spec.ts
@@ -1,55 +1,59 @@
-import { TestBed, ComponentFixture, async } from '@angular/core/testing';
-import { IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import {} from 'jasmine';
-
-
-
-let comp: MyApp;
-let fixture: ComponentFixture<MyApp>;
-
-describe('Component: Root Component', () => {
-
-    beforeEach(async(() => {
-
-        TestBed.configureTestingModule({
-
-            declarations: [MyApp],
-
-            providers: [
-
-            ],
-
-            imports: [
-                IonicModule.forRoot(MyApp)
-            ]
-
-        }).compileComponents();
-
-    }));
-
-    beforeEach(() => {
-
-        fixture = TestBed.createComponent(MyApp);
-        comp    = fixture.componentInstance;
-
+import { MyApp }                      from './app.component';
+import { MenuMock, NavMock, PlatformMock } from '../mocks';
+import { LoginPage } from '../pages/login/login';
+import { inject } from '@angular/core/testing';
+import { Events, Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+import { Provider } from '@angular/core';
+import {NavController,MenuController} from 'ionic-angular';
+// Mock out Ionic's platform class
+class MockClass {
+  public ready(): any {
+    return new Promise((resolve: Function) => {
+      resolve();
     });
+  }
+}
 
-    afterEach(() => {
-        fixture.destroy();
-        comp = null;
-    });
 
-    it('is created', () => {
+let instance: MyApp = null;
+let loginPage = null;
+let app: MyApp;
 
-        expect(fixture).toBeTruthy();
-        expect(comp).toBeTruthy();
 
-    });
+describe('App', () => {
 
-    it('initialises with a root page of HomePage', () => {
-        expect(comp['rootPage']).toBe(HomePage);
-    });
+  // provide our implementations or mocks to the dependency injector
+  let nav: NavController;
+  let menu: MenuController;
+   let platform;
+beforeEach(inject([Platform], (_platform: Platform) => {
+ platform = _platform;
+ spyOn(_platform, 'ready').and.callThrough();
 
+ spyOn(StatusBar, 'styleDefault');
+ spyOn(Splashscreen, 'hide');
+
+ app = new MyApp(platform,menu);
+}));
+
+  it('iniciar app', () => {
+  expect(app.initializeApp).not.toBe(null);
+});
+
+it('iniciar root page', () => {
+  expect(app.rootPage).not.toBe(null);
+});
+
+it('iniciar platform', () => {
+  expect(platform.ready).toHaveBeenCalled();
+});
+
+it('definir pages', () => {
+  expect(app.pages.length).toEqual(12);
+});
+
+it('abrir menu', () => {
+  expect(app.menu).not.toBe(null);
+});
 });
